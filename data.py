32bb1c88a2cf69226287bc7f8791ec4e8cd8cf17
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-02-08 20:10:51 -0500

    data.py to html

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
-import pandas as pandas
+import pandas as pd
 
 #Read the csv file
-df = pd.read_csv('..Resources/cities.csv')
+df = pd.read_csv('Resources/cities.csv')
 
 #Save to file
-dfto_html('data.html'. index=False)
+df.to_html('data.html', index=False)
 
 #Assign to string
 table = df.to_html()
